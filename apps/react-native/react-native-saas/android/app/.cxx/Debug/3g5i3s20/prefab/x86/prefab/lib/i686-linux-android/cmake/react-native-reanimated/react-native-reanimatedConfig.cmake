5a10b01f4ef34b81b34008218aabf0f9a20ffb9f
Edwin Lim <edwin@posthog.com>
2026-02-10 18:15:25 -0500

    react native suggestions

--- apps/react-native/react-native-saas/android/app/.cxx/Debug/3g5i3s20/prefab/x86/prefab/lib/i686-linux-android/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-reanimated::reanimated)
-add_library(react-native-reanimated::reanimated SHARED IMPORTED)
-set_target_properties(react-native-reanimated::reanimated PROPERTIES
-    IMPORTED_LOCATION "/Users/edwinyjlim/development/wizard-workbench/apps/react-native/react-native-saas/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/4i1i176q/obj/x86/libreanimated.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/edwinyjlim/development/wizard-workbench/apps/react-native/react-native-saas/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
