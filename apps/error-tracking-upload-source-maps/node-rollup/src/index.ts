e135a5a97c375e114542828c53d1100f06fca203
ablaszkiewicz <kqmdjc8@gmail.com>
2026-05-29 16:48:14 +0200

    fix: node rollup case

--- apps/error-tracking-upload-source-maps/node-rollup/src/index.ts
+++ apps/error-tracking-upload-source-maps/node-rollup/src/index.ts
@@ -7,6 +7,4 @@ const client = new PostHog(process.env.POSTHOG_KEY ?? '', {
 
 client.capture({ distinctId: 'node-rollup', event: 'hello' })
 
-client.captureException(new Error('PostHog test error - 2026-05-26T00:00:00.000Z'), 'asodsd', { $exception_fingerprint: '342434' })
-
 await client.shutdown()
