3cc5bf06ed67675d0821fa7867aa120a5805b113
ablaszkiewicz <kqmdjc8@gmail.com>
2026-05-29 13:58:18 +0200

    fix: node raw

--- apps/error-tracking-upload-source-maps/node-raw/src/index.ts
+++ apps/error-tracking-upload-source-maps/node-raw/src/index.ts
@@ -6,14 +6,5 @@ const client = new PostHog(process.env.POSTHOG_KEY ?? '', {
 
 client.capture({ distinctId: 'node-raw', event: 'hello' })
 
-function throwTestError() {
-  throw new Error('PostHog test error - 2026-05-29T00:00:00.000Z')
-}
-
-try {
-  throwTestError()
-} catch (error) {
-  client.captureException(error, 'node-raw', { $exception_fingerprint: '342434' })
-}
 
 await client.shutdown()
