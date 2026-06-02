d2909dd91db6b5228669a44dc3387637b666d306
ablaszkiewicz <kqmdjc8@gmail.com>
2026-06-02 17:43:17 +0200

    feat: clear nuxt config

--- apps/error-tracking-upload-source-maps/nuxt-4-3/nuxt.config.ts
+++ apps/error-tracking-upload-source-maps/nuxt-4-3/nuxt.config.ts
@@ -1,29 +1,3 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-01-01",
-  modules: ["@posthog/nuxt"],
-  sourcemap: {
-    client: "hidden",
-  },
-  nitro: {
-    rollupConfig: {
-      output: {
-        sourcemapExcludeSources: false,
-      },
-    },
-  },
-  posthogConfig: {
-    publicKey: process.env.NUXT_PUBLIC_POSTHOG_KEY ?? "",
-    host: process.env.NUXT_PUBLIC_POSTHOG_HOST ?? "",
-    clientConfig: {
-      capture_exceptions: true,
-    },
-    serverConfig: {
-      enableExceptionAutocapture: true,
-    },
-    sourcemaps: {
-      enabled: true,
-      projectId: process.env.POSTHOG_PROJECT_ID ?? "",
-      personalApiKey: process.env.POSTHOG_API_KEY ?? "",
-    },
-  },
 });
